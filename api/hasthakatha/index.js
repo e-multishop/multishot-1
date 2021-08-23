1c2749474b1f6bcb53d1fdfe062b456d485d0cf2
Manoj Baruah <manoj.adams@gmail.com>
2021-08-23 15:26:46 +0530

    update folder structure

---
+++ api/hasthakatha/index.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "hasthakatha"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+const port = 3000;
+const hasthakatha_app = require('./src/app');
+const login_app = require('./src/common/login');
+const common_app = require('./src/common/common');
+const product_app = require('./src/admin/product');
+const hasthakatha_data=hasthakatha_app();
+login_app(hasthakatha_data,con);
+product_app(hasthakatha_data,con);
+common_app(hasthakatha_data,con);
+hasthakatha_data.listen(port);
