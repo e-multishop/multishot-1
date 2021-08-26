9aa23d79a99485eea34a657e3e03f75c02c6a3dd
Manoj Baruah <manoj.adams@gmail.com>
2021-08-26 11:28:39 +0530

    update admin gulp commands

--- ui/admin_dashboard/gulpfile.js
+++ ui/admin_dashboard/gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('updateReferences', function() {
 });
 
 gulp.task("copyAssets", function() {
-  return gulp.src('./build/*')
+  return gulp.src('./build/*/**')
     .pipe(gulp.dest('./../../api/hasthakatha/public/admin/'));
 });
 
