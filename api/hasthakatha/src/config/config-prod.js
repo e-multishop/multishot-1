8c32d892fa48d7a500e5e66e418fce4523e7d424
Manoj Baruah <manoj.adams@gmail.com>
2021-08-26 14:39:12 +0530

    update configuration data

--- api/hasthakatha/src/config/config-prod.js
+++ api/hasthakatha/src/config/config-prod.js
@@ -2,7 +2,7 @@ module.exports = {
     port: process.env.PORT,
     USER: 'manbatest123@gmail.com',
     PASS: 'hereiammanoj',
-    db_host: "www.db4free.net/",
+    db_host: "db4free.net",
     db_user: "admin_has_123",
     db_password: "Welcome@123",
     db_database: "hastakatha_app"
