766bb0a289f6b6be83e7520decc4540ce19bd473
Manoj Baruah <manoj.adams@gmail.com>
2021-09-01 20:18:02 +0530

    adding loader

--- ui/hasthakatha/src/Components/Pages/login/Forgotpassword.js
+++ ui/hasthakatha/src/Components/Pages/login/Forgotpassword.js
@@ -3,23 +3,25 @@ import Axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faCheck} from '@fortawesome/free-solid-svg-icons' 
+import Loader from '../../Common/Loader';
 
 function Forgotpassword(props) {
     const [email, setEmail] = useState('');
     const [status, setStatus] = useState('');
-    
+    const [loader, setLoader] = useState(false);
     const handleChange = (e) => {
         setEmail(e.target.value);
         setStatus('')
     }
     const onSubmit = () => {
-
+        setLoader(true);
         Axios.post("/rest/forgot_password", {
             email: email
         }).then(res => {
             // console.warn("value is success fill")
             // consosle.warn(data.token);
             setStatus('')
+            setLoader(false);
             toast.info(<span >
                 <FontAwesomeIcon icon={ faCheck} size='lg' color="white" className="icon toast-icon" />  
                 If you are registered with us, you should get an email containing instructions to reset your password
@@ -29,15 +31,15 @@ function Forgotpassword(props) {
         }).catch(err => {
             // console.warn(err);
             // setStatus('Email is not registred')
+            setLoader(false);
             props.closeModal();
-
             toast.info("If you are registered with us, you should get an email containing instructions to reset your password")
 
 
         });
     }
-    return (
-        <div className="row no-margin ">
+    const show = () => {
+        return (
             <form className="signup-form">
                 <h5>Forgot Password</h5>
                 <div className="input-field">
@@ -49,9 +51,14 @@ function Forgotpassword(props) {
                 <p className="status">{status}</p>
                 <div className="button center-align">
                 <a className="waves-effect waves-light btn signup-btn" onClick={()=>onSubmit()}>Submit</a>
-                       </div>
+                    </div>
             </form>
-
+        )
+    };
+    return (
+        <div className="row no-margin hk-forgot-password wrapper">
+            { loader ? <Loader height="250px" /> : ''}
+            { show() }
         </div>
     );
 }
--- ui/hasthakatha/src/Components/Pages/login/Login.js
+++ ui/hasthakatha/src/Components/Pages/login/Login.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import "./login.scss";
 // import Modal from "./Modal"
 import Axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faCheck} from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
+import Loader from '../../Common/Loader';
 // import { useEffect } from 'react/cjs/react.production.min';
 // import { useParams } from 'react-router';
 
@@ -12,9 +13,10 @@ const Login = (props) => {
     // const redirect_path=useParams();
     const [login, setLogin] = useState({
         hklogin_emailid: '',
-        password: '',
+        password: ''
     });
     const [status, setStatus] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleChange = (e) => {
         setLogin({ ...login, [e.target.id]: e.target.value });
@@ -23,6 +25,7 @@ const Login = (props) => {
     const onSubmit = (event) => {
         event.stopPropagation();
         event.preventDefault();
+        setLoading(true);
         Axios.post("/rest/login", {
             email: login.hklogin_emailid,
             password: login.password
@@ -31,6 +34,7 @@ const Login = (props) => {
             const token = res.data.token;
             localStorage.setItem('token', token);
             setStatus('')
+            setLoading(false);
             // console.log("email",login.email)
             if (props && props.closeModal) {
                 props.closeModal(true);
@@ -41,6 +45,7 @@ const Login = (props) => {
             toast.success(<span ><FontAwesomeIcon icon={ faCheck} size='lg' color="white" className="icon toast-icon" />  Success</span>)
             // props.history.push("/about"); 
         }).catch(err => {
+            setLoading(false);
             console.warn(err);
             setStatus('Email & password did not match')
         });
@@ -50,48 +55,56 @@ const Login = (props) => {
             props.setVeiwstatus(value)
         }
     }
-    return (
-        <div>
-            <form className=" hk-loginform">
-                <h5>Sign-In</h5>
-                <div className=" hk-offsetmd">
-                    <div className="hk-card">
-                        <div class="card-content">
-                            <div className="input-field">
-                                <input id="hklogin_emailid" type="email" className="validate" value={login.hklogin_emailid} onChange={(e) => handleChange(e)} />
-                                <label for="hklogin_emailid">Email</label>
-                                <span className="helper-text" data-error="Email is not valid" ></span>
-                            </div>
-                            <div className="input-field ">
-                                <input id="password" type="password" value={login.password} onChange={(e) => handleChange(e)} />
-                                <label for="password">Password</label>
-                            </div>
-                            <div className="input-field">
-                                <p>
-                                    <label>
-                                        <input type="checkbox" />
-                                        <span>Keep me logged in </span>
-                                    </label>
-                                    <span className="right forgot">
-                                        <button onClick={() => { setViewStatus('forgotpassword') }}
-                                        >Forgot Password?
-                                        </button>
-                                    </span>
-                                </p>
-                            </div>
-                            <p className="status">{status}</p>
-                            <div className="button center-align">
-                                <button class="waves-effect waves-light btn sign-in-btn" onClick={(e) => { onSubmit(e) }}>SIGN IN</button>
-                            </div>
-                            <div className="join-now center-align">
-                                Not yet a member?<button
-                                    onClick={() => { setViewStatus('signup') }}
-                                >JOIN NOW</button>
+    var show = () => {
+        return (
+            <Fragment>
+                <form className=" hk-loginform">
+                    <h5>Sign-In</h5>
+                    <div className=" hk-offsetmd">
+                        <div className="hk-card">
+                            <div class="card-content">
+                                <div className="input-field">
+                                    <input id="hklogin_emailid" type="email" className="validate" value={login.hklogin_emailid} onChange={(e) => handleChange(e)} />
+                                    <label for="hklogin_emailid" className="">Email</label>
+                                    <span className="helper-text" data-error="Email is not valid" ></span>
+                                </div>
+                                <div className="input-field">
+                                    <input id="password" type="password" value={login.password} onChange={(e) => handleChange(e)} />
+                                    <label for="password">Password</label>
+                                </div>
+                                <div className="input-field">
+                                    <p>
+                                        <label>
+                                            <input type="checkbox" />
+                                            <span>Keep me logged in </span>
+                                        </label>
+                                        <span className="right forgot">
+                                            <button onClick={() => { setViewStatus('forgotpassword') }}
+                                            >Forgot Password?
+                                            </button>
+                                        </span>
+                                    </p>
+                                </div>
+                                <p className="status">{status}</p>
+                                <div className="button center-align">
+                                    <button class="waves-effect waves-light btn sign-in-btn" onClick={(e) => { onSubmit(e) }}>SIGN IN</button>
+                                </div>
+                                <div className="join-now center-align">
+                                    Not yet a member?<button
+                                        onClick={() => { setViewStatus('signup') }}
+                                    >JOIN NOW</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </Fragment>
+        );
+    }
+    return (
+        <div className="hk-login wrapper">
+            { loading ? <Loader height="250px"/> : '' }
+            { show() }
         </div>
     );
 
