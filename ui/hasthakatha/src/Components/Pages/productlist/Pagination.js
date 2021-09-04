2c0f76e538230de88141d97c54e3a501bd3512f4
Shiv Prasad <shivprasad721@gmail.com>
2021-09-04 14:40:48 +0530

    pagination ui add and loading on product page

--- ui/hasthakatha/src/Components/Pages/productlist/Pagination.js
+++ ui/hasthakatha/src/Components/Pages/productlist/Pagination.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight,faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 
-
-const Pagination =()=>{
-    return(
-        <ul className="pagination">
-    <li className="disabled"><a href="#!"><i className="material-icons">chevron_left</i></a></li>
-    <li className="active"><a href="#!">1</a></li>
-    <li className="waves-effect"><a href="#!">2</a></li>
-    <li className="waves-effect"><a href="#!">3</a></li>
-    <li className="waves-effect"><a href="#!">4</a></li>
-    <li className="waves-effect"><a href="#!">5</a></li>
-    <li className="waves-effect"><a href="#!"><i className="material-icons">chevron_right</i></a></li>
-  </ul>
-    );
+const Pagination = () => {
+  return (
+    <ul className="pagination">
+      <li className="disabled"><a href="#!"><FontAwesomeIcon icon={faChevronLeft} size="large"/></a></li>
+      <li className="active"><a href="#!">1</a></li>
+      <li className="waves-effect"><a href="#!">2</a></li>
+      <li className="waves-effect"><a href="#!">3</a></li>
+      <li className="waves-effect"><a href="#!">4</a></li>
+      <li className="waves-effect"><a href="#!">5</a></li>
+      <li className="waves-effect"><a href="#!"><FontAwesomeIcon icon={faChevronRight} size="large"/></a></li>
+    </ul>
+  );
 }
 
 export default Pagination;
--- ui/hasthakatha/src/Components/Pages/productlist/ProductList.js
+++ ui/hasthakatha/src/Components/Pages/productlist/ProductList.js
@@ -3,6 +3,8 @@ import "./ProductList.scss";
 import { NavLink } from 'react-router-dom';
 import { addToCart } from '../../../Redux/actions/index';
 import { useDispatch } from 'react-redux';
+import Pagination from './Pagination'
+import Loader from "../../Shared/loader/Loader"
 
 const ProductList = () => {
 
@@ -12,6 +14,7 @@ const ProductList = () => {
     // };
     const [categories, setCategories] = useState([]);
     const [product, setProduct] = useState([]);
+    const [Loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('rest/categories').then((result) => {
@@ -21,10 +24,13 @@ const ProductList = () => {
         })
         fetch('/rest/product_list').then((result) => {
             return (result.json())
-        }).then((product) => { setProduct(product) })
+        }).then((product) => {
+            setProduct(product)
+            setLoading(false);
+        })
     }, []);
-    const image_url =(image_data)=>{
-        var image_url="data:image/png;base64,"+(image_data);  
+    const image_url = (image_data) => {
+        var image_url = "data:image/png;base64," + (image_data);
         return (<img src={image_url} />);
     }
 
@@ -34,74 +40,86 @@ const ProductList = () => {
         description: "check description",
         price: "check price"
     }
-    console.warn("checku url",image_url);
+    // console.warn("checku url", image_url);
 
     return (
         <>
+
             {/* shop badge */}
             <div className="shop-badge">
                 Shop
-                </div>
-            <div className="hk-filter">
-                <div className=" heading">Items</div>
-                <div className=" button-filter">
-                    Short : most recent
-            </div>
-            </div>
-            {/* <!--product main_list--> */}
-            <div class="hs-container">
-                {/* <!--product side list category--> */}
-                <div class="side_category">
-                    <div>All</div>
-                    {
-                        categories.map((value) => {
-                            return (<div>{value.name}</div>)
-                        })
-                    }
-                </div>
-                {/* <!-- product List--> */}
-                {/* <div> */}
-                <div class="product_list">
-                    {/* <!-- Prodcuct list first row--> */}
-                    {/* <!--product details--> */}
-                    {product.map((value) => {
-                        return (
-                            <>
-                                <div className="hk-product_card">
-                                    <NavLink to="/productdetails">
-                                        <div className="img-wraper">
-                                            {/* {const url= atob(value.url)} */}
-                                            {
-                                                value.image_data?image_url(value.image_data):""
-                                            }
-                                            {/* <p>{avalue.image_data}</p> */}
-                                            {/* <img src={image_url} /> */}
-                                        </div>
-                                        <div className="description">
-                                            {value.title}
-                                        </div>
-                                        <div className="price">
-                                            {value.price}
-                                        </div>
-                                    </NavLink>
-                                    <div className="hk-addcard" onClick={() => { dispatch(addToCart(data)) }}>
-                                        <a>ADD TO CART</a>
-                                    </div>
-                                </div>
-                                {/* <div className="row">
+                    </div>
+            {Loading ?
+                <div className="loader">
+                    <Loader />
+                </div> :
+                <div>
+                    <div className="hk-filter">
+                        <div className=" heading">Items</div>
+                        <div className=" button-filter">
+                            Short : most recent
+                        </div>
+                    </div>
+                    {/* <!--product main_list--> */}
+
+                    <div class="hs-container">
+                        {/* <!--product side list category--> */}
+                        <div class="side_category">
+                            <div>All</div>
+                            {
+                                categories.map((value) => {
+                                    return (<div>{value.name}</div>)
+                                })
+                            }
+                        </div>
+                        {/* <!-- product List--> */}
+                        {/* <div> */}
+                        <div className="product_list_container">
+                            <div class="product_list">
+                                {/* <!-- Prodcuct list first row--> */}
+                                {/* <!--product details--> */}
+                                {product.map((value) => {
+                                    return (
+                                        <>
+                                            <div className="hk-product_card">
+                                                <NavLink to="/productdetails">
+                                                    <div className="img-wraper">
+                                                        {/* {const url= atob(value.url)} */}
+                                                        {
+                                                            value.image_data ? image_url(value.image_data) : ""
+                                                        }
+                                                        {/* <p>{avalue.image_data}</p> */}
+                                                        {/* <img src={image_url} /> */}
+                                                    </div>
+                                                    <div className="description">
+                                                        {value.title}
+                                                    </div>
+                                                    <div className="price">
+                                                        {value.price}
+                                                    </div>
+                                                </NavLink>
+                                                <div className="hk-addcard" onClick={() => { dispatch(addToCart(data)) }}>
+                                                    <a>ADD TO CART</a>
+                                                </div>
+                                            </div>
+                                            {/* <div className="row">
                                 <div className="col s3">
                                     <Card images={value.url} banner={true} title={value.title} description={value.price}/>
                                 </div>
                             </div> */}
-                            </>
-                        );
-                    })
-                    }
-                </div>
-                {/* </div> */}
-                {/* <Pagination/> */}
 
-            </div>
+                                        </>
+                                    );
+                                })
+                                }
+                            </div>
+                            <div className="center-align pagination">
+                                <Pagination />
+                            </div>
+                        </div>
+                        {/* </div> */}
+                    </div>
+                </div>}
         </>
     );
 }
