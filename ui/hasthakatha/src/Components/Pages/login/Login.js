f318a5eec38cd654886ecb740874789e7b9bb533
Shiv Prasad <shivprasad721@gmail.com>
2021-09-07 20:50:03 +0530

    working on reset password

--- ui/hasthakatha/src/Components/Pages/login/Login.js
+++ ui/hasthakatha/src/Components/Pages/login/Login.js
@@ -27,7 +27,6 @@ const Login = (props) => {
         event.preventDefault();
         setLoading(true);
         Axios.post("/rest/login", {
-            email: login.hklogin_emailid,
             password: login.password
         }).then(res => {
             // console.warn("value is success fill",res.data)
