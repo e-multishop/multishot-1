262cf68b218ac9030428a6d7658d3f2a83975b8c
Shiv Prasad <shivprasad721@gmail.com>
2021-10-11 22:00:46 +0530

    user profile ui complete

--- ui/hasthakatha/src/Components/Pages/User_dashboard/account/Account.js
+++ ui/hasthakatha/src/Components/Pages/User_dashboard/account/Account.js
@@ -4,26 +4,27 @@ import Header from '../../../Header/Header';
 import Footer from '../../../Footer/Footer';
 import Address_list from '../address/Address_list'
 import { NavLink } from 'react-router-dom';
+import UserProfile from '../profile/UserProfile';
 
 function Account() {
     // const cartData = useSelector((state) => state.cartItems);
     // console.log("cart data check =", cartData);
     return (
         <>
-        <Header/>
-        <div className="hk-container">
-            <div>
+            <Header />
+            <div className="hk-container">
+                <div>
                     <div className="row">
                         <div className="col s3">
-                            <Sidebar/>
+                            <Sidebar />
                         </div>
                         <div className="col s9">
-                        <Address_list/>
+                            <UserProfile/>
                         </div>
                     </div>
                 </div>
-        </div>
-        <Footer/>
+            </div>
+            <Footer />
         </>
     );
 }
