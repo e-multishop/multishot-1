c3afdb8d1ec1659c7e4b3015ac5d473d8d4c3dcc
ashish <ashishmaurya7054@gmail.com>
2021-11-01 21:22:42 +0530

    addding console.err

--- api/hasthakatha/src/common/common.js
+++ api/hasthakatha/src/common/common.js
@@ -12,7 +12,7 @@ var common_app=function(app,con)
         var sql = "SELECT name, cid FROM `category`";
         con.query(sql, function (err, result) {
             if (err) throw err;
-            console.error(JSON.stringify(result));
+            console.log(JSON.stringify(result));
             res.send(JSON.stringify(result));
         });
     });
