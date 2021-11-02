97642ed3bf3fda93e71d53005674c95f69dbf09e
shivprasad721 <shivprasad721@gmail.com>
2021-11-02 22:36:27 +0530

    home page work

--- ui/hasthakatha/src/Components/Pages/Home/Team.js
+++ ui/hasthakatha/src/Components/Pages/Home/Team.js
@@ -1,72 +1,97 @@
 import React from 'react';
 import CardTeam from '../../TeamCard/CardTeam';
-import imgcheck from "../../../Images/megha.jpg";
+import tanisha_kumari from "../../../Images/tanisha_kumari.jpg";
+import ruchi_choudhary from "../../../Images/ruchi_choudhary.jpg";
+import ekta from "../../../Images/ekta.jpg";
+import divya from "../../../Images/divya.jpg";
 
-const Team =()=>{
-    const TeamData=[
+const Team = () => {
+    const TeamData1 = [
         {
-            img:imgcheck,
-            name:"Team Member Name",
-            designation:"Founder",
-            description:"I am a very simple card. I am good at containing small bits of information.I am good at containing small bits of information.I am good at containing small bits of information.",
-            fblink:"https://www.facebook.com/Hastha-Katha-1102781866557529/",
-            twitterlink:"https://twitter.com/hastha_katha",
-            instalink:"https://www.instagram.com/hastha_katha/?hl=en"
+            img: divya,
+            name: "Divya Lakshmi",
+            designation: "Founder",
+            description: "",
+            fblink: "https://www.facebook.com/Hastha-Katha-1102781866557529/",
+            twitterlink: "https://twitter.com/hastha_katha",
+            instalink: "https://www.instagram.com/hastha_katha/?hl=en"
 
         },
         {
-            img:imgcheck,
-            name:"Team Member Name",
-            designation:"Founder",
-            description:"I am a very simple card. I am good at containing small bits of information.I am good at containing small bits of information.I am good at containing small bits of information.",
-            fblink:"https://www.facebook.com/Hastha-Katha-1102781866557529/",
-            twitterlink:"https://twitter.com/hastha_katha",
-            instalink:"https://www.instagram.com/hastha_katha/?hl=en"
+            img: ekta,
+            name: "Ekta Jaiswal",
+            designation: "Creative Head",
+            description: "",
+            fblink: "https://www.facebook.com/Hastha-Katha-1102781866557529/",
+            twitterlink: "https://twitter.com/hastha_katha",
+            instalink: "https://www.instagram.com/hastha_katha/?hl=en"
+        }
+    ];
+    const TeamData2 = [
+        {
+            img: ruchi_choudhary,
+            name: "Ruchi Choudhary",
+            designation: "Home Decor",
+            description: "",
+            fblink: "https://www.facebook.com/Hastha-Katha-1102781866557529/",
+            twitterlink: "https://twitter.com/hastha_katha",
+            instalink: "https://www.instagram.com/hastha_katha/?hl=en"
         },
         {
-            img:imgcheck,
-            name:"Team Member Name",
-            designation:"Founder",
-            description:"I am a very simple card. I am good at containing small bits of information.I am good at containing small bits of information.I am good at containing small bits of information.",
-            fblink:"https://www.facebook.com/Hastha-Katha-1102781866557529/",
-            twitterlink:"https://twitter.com/hastha_katha",
-            instalink:"https://www.instagram.com/hastha_katha/?hl=en"
-        },
-        {
-            img:imgcheck,
-            name:"Team Member Name",
-            designation:"Founder",
-            description:"I am a very simple card. I am good at containing small bits of information.I am good at containing small bits of information.I am good at containing small bits of information.",
+            img:tanisha_kumari,
+            name:"Tanisha Kumari",
+            designation:"Social Media Manager",
+            description:"",
             fblink:"https://www.facebook.com/Hastha-Katha-1102781866557529/",
             twitterlink:"https://twitter.com/hastha_katha",
             instalink:"https://www.instagram.com/hastha_katha/?hl=en"
         }
     ];
+    
     return (
         <>
-        <div>
-            <h2 className="hk-feature">Our Team</h2>
-        </div>
-        <div className="row ">
-            {
-                TeamData.map((value)=>{
-                    return(
-                        <div className="col s6 hk-team-col">
-                            <CardTeam 
-                                images={value.img} 
-                                name={value.name} 
-                                designation={value.designation}
-                                description={value.description}
-                                fblink={value.fblink}
-                                twitterlink={value.twitterlink}
-                                instalink={value.instalink}
-                            />
-                        </div>
-                    );
-                })
-            }
-        </div>
-    </>
+            <div>
+                <h2 className="hk-feature">Our Team</h2>
+            </div>
+            <div className="row">
+                {
+                    TeamData1.map((value) => {
+                        return (
+                                    <div className="col s6 hk-team-col">
+                                    <CardTeam
+                                        images={value.img}
+                                        name={value.name}
+                                        designation={value.designation}
+                                        description={value.description}
+                                        fblink={value.fblink}
+                                        twitterlink={value.twitterlink}
+                                        instalink={value.instalink}
+                                    />
+                                </div>
+                        );
+                    })
+                }
+            </div>
+            <div className="row">
+                {
+                    TeamData2.map((value) => {
+                        return (
+                                    <div className="col s6 hk-team-col">
+                                    <CardTeam
+                                        images={value.img}
+                                        name={value.name}
+                                        designation={value.designation}
+                                        description={value.description}
+                                        fblink={value.fblink}
+                                        twitterlink={value.twitterlink}
+                                        instalink={value.instalink}
+                                    />
+                                </div>
+                        );
+                    })
+                }
+            </div>
+        </>
     );
 }
 
